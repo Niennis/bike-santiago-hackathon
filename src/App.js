a3945d9beee30e67cb49cba93c8a50aa74a02e49
conois <conii.echeverria@gmail.com>
2018-03-22 12:04:58 -0300

    Instalando bootstrap, implementando modal de informacion para market

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import Demo from './components/geolocation';
 import logo from './logo.svg';
 import './App.css';
+
+//Component
+import Demo from './components/geolocation';
+import ModalStation from './components/resumenStation'; 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Demo/>
+        <ModalStation />
       </div>
     );
   }
