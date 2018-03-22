ba239a318c45499fa58d5f4fd1bcbb06b37003a9
Niennis <estefania.osses.v@gmail>
2018-03-22 16:19:20 -0300

    Barra de navegación agregada

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link, Redirect, withRouter, Switch } fr
 import { Navbar, Header, Nav, Brand, NavItem, NavDrop} from 'react-bootstrap';
 import BikeIcon from './../img/logo_bike.png';
 import Example from './resumenStation';
+import './navigationBar.css'
 
 class NavigationBar extends Component {
   render() {
@@ -11,19 +12,13 @@ class NavigationBar extends Component {
         <div>
                 {/* <NavItem><Link to="/SearchImg" className="links">Images</Link></NavItem> */}
           <Navbar className="fixedBottom">
-            <Navbar.Header>
-              <Navbar.Brand>
+              {/* <Navbar.Brand>
                 <img src={BikeIcon}/>
-              </Navbar.Brand>
-            </Navbar.Header>
-            <Nav>
-              <NavItem>
-                <Link to="/Example" className="links">Example</Link>
-              </NavItem>
-              <NavItem>
-                Link
-              </NavItem>
-            </Nav>
+              </Navbar.Brand>             */}
+            <Link to="/Example" className="links"><i className="fas fa-align-right fa-2x"></i></Link>
+            <Link to="/Example" className="links"><i className="fas fa-map-marker-alt fa-2x"></i></Link>
+            <Link to="/Example" className="links"><i className="fas fa-user fa-2x"></i></Link>
+            <Link to="/Example" className="links"><i className="fas fa-phone-volume fa-2x"></i></Link>
           </Navbar>
 
           <Route path="/Example" component={Example}></Route>
