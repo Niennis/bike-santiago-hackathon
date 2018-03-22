8aeb26d406eb5e270832824af933693f371042cf
conois <conii.echeverria@gmail.com>
2018-03-22 13:24:52 -0300

    agregando options de report

--- src/components/resumenStation.js
+++ src/components/resumenStation.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import { Modal } from 'react-bootstrap';
 import { Label } from 'react-bootstrap'; 
-import Estacionamiento from './../img/estacion-salvador-bikesantiago.jpg'; 
+import Estacionamiento from './../img/estacion-salvador-bikesantiago.jpg';
 import './resumenStation.css';
 
-
-class Example extends React.Component {
+class Example extends Component {
   constructor(props, context) {
     super(props, context);
 
     this.handleShow = this.handleShow.bind(this);
     this.handleClose = this.handleClose.bind(this);
 
+
     this.state = {
-      show: false
+      show: false, 
+      showFooter: false, 
     };
   }
 
@@ -40,10 +41,19 @@ class Example extends React.Component {
           <Modal.Body>
           	<h3>Nombre Estacionamiento</h3>
           	<h5>Sub Nombre Estacionamiento</h5>
-          	<Label className="label-info">Parking</Label>
-          	<Label className="label-info">Biclycle</Label>
-           
+          	<Label className="label-info">Parking<span>5</span></Label>
+          	<Label className="label-info">Biclycle<span>5</span></Label>
+          	<Button className="info-reportar">Reportar</Button>       
           </Modal.Body>
+          <Modal.Footer>
+          	<h4>Seleccione el problema que necesita reportar</h4>
+          	<select name="carlist" form="carform">
+						  <option value="frenos">Frenos Malos</option>
+						  <option value="pinchar">Bicicleta Pinchada</option>
+						  <option value="luz">Luz Led mala</option>
+						  <option value="otro">Otro</option>
+						</select>
+          </Modal.Footer>
         </Modal>
       </div>
     );
@@ -51,3 +61,18 @@ class Example extends React.Component {
 }
 
 export default Example
+
+/*
+    this.handleCloseFooter = this.handleCloseFooter.bind(this); 
+    this.handleShowFooter = this.handleShowFooter.bind(this); 
+
+  handleCloseFooter(){
+  	this.setState({ showFooter: false});
+  }
+
+  handleShowFooter(){
+  	this.setState({ showFooter: true}); 
+  }
+
+showFooter={this.state.showFooter} onHideFooter={this.handleCloseFooter}
+*/
